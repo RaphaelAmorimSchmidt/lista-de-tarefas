12710d6abf0022f53607430e9d0328207bb78cc9
RaphaelAmorim Schmidt <raphaschmidt89@gmail.com>
2019-10-24 17:40:41 -0300

    add list model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_24_191204) do
+ActiveRecord::Schema.define(version: 2019_10_24_203818) do
+
+  create_table "lists", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "name"
+    t.boolean "public"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_lists_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
