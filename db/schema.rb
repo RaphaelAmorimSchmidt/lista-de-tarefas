ffd9893be01785edf403b93f4137e7a6aa67fe1f
RaphaelAmorim Schmidt <raphaschmidt89@gmail.com>
2019-10-24 17:55:44 -0300

    add favorite model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_24_204312) do
+ActiveRecord::Schema.define(version: 2019_10_24_205255) do
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "list_id"
+    t.boolean "check"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["list_id"], name: "index_favorites_on_list_id"
+    t.index ["user_id"], name: "index_favorites_on_user_id"
+  end
 
   create_table "lists", force: :cascade do |t|
     t.integer "user_id"
